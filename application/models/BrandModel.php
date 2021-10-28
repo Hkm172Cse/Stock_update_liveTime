de940252628857d177d102f819cc46ce01747a17
Hkm172Cse <azizulhakim000111@gmail.com>
2021-10-28 07:39:24 +0600

    All filed and folder added together

---
+++ application/models/BrandModel.php
@@ -0,0 +1,31 @@
+<?php
+    class BrandModel extends CI_Model {
+
+       
+        
+        public function insert_entry($data)
+        {
+               $query = "INSERT INTO `table_brand` (`brand`) VALUES ('$data')";
+              
+                return $this->db->query($query);
+        }
+
+        public function BrandSelectMethod(){
+               
+               $query = $this->db->query("select * from table_brand order by id DESC");
+               return $query->result(); 
+        }
+
+        public function update_entry()
+        {
+                $this->title    = $_POST['title'];
+                $this->content  = $_POST['content'];
+                $this->date     = time();
+
+                $this->db->update('entries', $this, array('id' => $_POST['id']));
+        }
+
+}
+
+
+?>
